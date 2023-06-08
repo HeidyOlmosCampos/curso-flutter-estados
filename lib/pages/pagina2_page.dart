f0e0f233943c5cc1bd6a86bbd5a883a9dfdeb8c4
Heidy Olmos <olmoscamposh@gmail.com>
2023-06-07 23:17:37 -0400

    Diseno listo

---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+  // const Pagina2Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina 2'),
+      ),
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MaterialButton(
+            child: Text('Establecer Usuario', style: TextStyle(color: Colors.white),),
+            color: Colors.blue,
+            onPressed: () {}, 
+          ),
+          MaterialButton(
+            child: Text('Cambiar edad', style: TextStyle(color: Colors.white),),
+            color: Colors.blue,
+            onPressed: () {}, 
+          ),
+          MaterialButton(
+            child: Text('Añadir profesion', style: TextStyle(color: Colors.white),),
+            color: Colors.blue,
+            onPressed: () {}, 
+          ),
+        ],
+      )),
+    );
+  }
+}
